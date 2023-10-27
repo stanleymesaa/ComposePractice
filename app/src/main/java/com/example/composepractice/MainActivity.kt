b9104c81b5fcf763197f39382109befc61dcaaf3
stanleymesa <stanleymesa2001@gmail.com>
2023-10-27 15:15:35 +0700

    update

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -1,7 +1,9 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.composepractice
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,8 +11,6 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -19,9 +19,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -37,27 +45,36 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.LifecycleCoroutineScope
-import androidx.lifecycle.lifecycleScope
 import com.example.composepractice.ui.theme.MainViewModel
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
     private val viewModel: MainViewModel by viewModels()
 
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .padding(16.dp)
+            Scaffold(
+                snackbarHost = { SnackbarHost(hostState = viewModel.snackbarHostState) }
             ) {
-                ImageCard()
-                Spacer(modifier = Modifier.height(16.dp))
-                ColorBox(
-                    viewModel = viewModel,
-                )
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                ) {
+                    Box(Modifier.fillMaxWidth(0.5f)) {
+                        ImageCard()
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Box(Modifier.fillMaxWidth(0.5f)) {
+                        ColorBox(viewModel = viewModel)
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    GreetMeTextField(viewModel = viewModel)
+                }
             }
         }
     }
@@ -123,9 +140,7 @@ fun ImageCard(
 
 @OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
 @Composable
-fun ColorBox(
-    viewModel: MainViewModel,
-) {
+fun ColorBox(viewModel: MainViewModel) {
     Box(
         Modifier
             .fillMaxWidth()
@@ -145,5 +160,28 @@ fun ColorBox(
                 true
             }
     ) {
+    }
+}
+
+@Composable
+fun GreetMeTextField(viewModel: MainViewModel) {
+    val scope = rememberCoroutineScope()
+
+    Column {
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = viewModel.name.value,
+            onValueChange = {
+                viewModel.name.value = it
+            },
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            scope.launch {
+                viewModel.snackbarHostState.showSnackbar("Hi ${viewModel.name.value}!")
+            }
+        }) {
+            Text(text = "Greet Me!")
+        }
     }
 }
--- app/src/main/java/com/example/composepractice/ui/theme/MainViewModel.kt
+++ app/src/main/java/com/example/composepractice/ui/theme/MainViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.composepractice.ui.theme
 
 import android.util.Log
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -12,8 +14,14 @@ import kotlinx.coroutines.launch
 import java.util.Random
 
 class MainViewModel: ViewModel() {
+
+    // Color
     val color = mutableStateOf(Color.Yellow)
     private var job: Job? = null
+
+    // Text Field
+    val name by lazy { mutableStateOf("") }
+    val snackbarHostState = SnackbarHostState()
 
     fun runChangeColor() {
         job = viewModelScope.launch {
